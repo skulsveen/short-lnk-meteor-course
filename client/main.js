3601af98efac2547ac105d9f025145e2d837c3dd
Sebastian Kulsveen <skulsveen@gmail.com>
2017-12-04 21:27:27 +0100

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,19 @@
+import 'babel-polyfill';
+import { Meteor } from 'meteor/meteor';
+import ReactDOM from 'react-dom';
+import { Tracker } from 'meteor/tracker';
+import { Session } from 'meteor/session';
+
+import { routes, onAuthChange } from '../imports/routes/routes';
+import '../imports/startup/simple-schema-configuration.js';
+
+
+Tracker.autorun(() => {
+  const isAuthenticated = !!Meteor.userId();
+  onAuthChange(isAuthenticated);
+});
+
+Meteor.startup(() => {
+  Session.set('showVisible', true);
+  ReactDOM.render(routes, document.getElementById('app'));
+});
